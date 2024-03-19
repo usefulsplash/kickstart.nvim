c79cf21111df2781edb2c3723a0c466222b6a2b5
usefulsplash <usefulsplash@proton.me>
2024-03-19 17:40:10 -0400

    enabled prettierd on markdown

--- init.lua
+++ init.lua
@@ -614,6 +614,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        markdown = { 'prettierd' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
