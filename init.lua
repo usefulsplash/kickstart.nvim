cfe3a89d0a24ba66cdbaa24f1ff262400ec69819
usefulsplash <usefulsplash@proton.me>
2024-05-29 14:57:31 -0400

    cleaning up

--- init.lua
+++ init.lua
@@ -735,6 +735,7 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    -- 'folke/tokyonight.nvim',
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
