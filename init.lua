9cc45807d344a81931e8bf70697a80da359deb71
usefulsplash <usefulsplash@proton.me>
2024-05-29 14:59:33 -0400

    made transparent

--- init.lua
+++ init.lua
@@ -738,6 +738,13 @@ require('lazy').setup({
     -- 'folke/tokyonight.nvim',
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
